b68a27dcee9da6f7e14e557bd92d23bff0ca9622
monkeesuit <sam640@nyu.edu>
2017-06-26 20:59:46 -0400

    Update fibonacci.py

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -1,8 +1,12 @@
 def fib(n):
+    
+    # Base case:
     if n == 0:
         return 0
     elif n == 1:
         return 1
+    
+    # Recursive case:
     else:
         return fib(n-1) + fib(n-2)
 
