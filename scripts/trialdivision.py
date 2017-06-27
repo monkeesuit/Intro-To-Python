548a98b94c78d1681d4e15ffdcac2967f0857631
monkeesuit <sam640@nyu.edu>
2017-06-26 21:06:47 -0400

    Update trialdivision.py

--- scripts/trialdivision.py
+++ scripts/trialdivision.py
@@ -1,11 +1,11 @@
 
 
 num = int(input('Enter a numer to be check for primality: '))
-ceiling = round(pow(num, .5)) + 1
+ceiling = int(pow(num, .5)) + 1										# Get ceiling of sqaure root of number (b/c of how range works 
 
 for i in range(2, ceiling):
-	if (num % i == 0):
-		print('{} is composite'.format(num))
+	if (num % i == 0):												# if any number between 2 and ceiling-1 divides num
+		print('{} is composite'.format(num))						# then num is a composite unmber
 		break
-else:
-		print('{} is prime'.format(num))
+else:																# if we make it through the loop then the number is prime
+	print('{} is prime'.format(num))
